1c8c0563518a75375c193dd2513b6d1f1a58cca6
RicardoRG96 <ricardoretamal10@gmail.com>
2024-11-26 17:43:52 -0300

    first commit

---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/oauthServer/OauthServerApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.scalable.ecommerce.platform.oauthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/oauthServer/OauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.scalable.ecommerce.platform.oauthServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
